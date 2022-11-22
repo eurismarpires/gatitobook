ea64cdbdfc0737067cd503d5d2fb3ae97095addd
Eurismar Pires Borges <eurismar.pires@totvs.com.br>
2022-11-21 21:20:17 -0300

    Criado o login e autenticação.

---
+++ src/app/autenticacao/autenticacao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const KEY = 'token';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AutenticacaoModule {
+
+}
---
+++ src/app/componentes/mensagem/mensagem.module.ts
@@ -0,0 +1,12 @@
+import { MensagemComponent } from './mensagem.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [MensagemComponent],
+  imports: [CommonModule],
+  exports: [MensagemComponent],
+})
+export class MensagemModule { }
